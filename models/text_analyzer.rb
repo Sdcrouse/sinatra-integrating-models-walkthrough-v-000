f18be3464b59396f75a6023e98fbf284431d6767
Steven Crouse <sdcrouse91@gmail.com>
2019-06-28 08:36:50 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -35,24 +35,26 @@ class TextAnalyzer
       letter_hash[letter] = arr.count(letter)
     end
     
-    # biggest = { letter_hash.keys.first => letter_hash[letter_hash.keys.first] }
+    biggest = { letter_hash.keys.first => letter_hash[letter_hash.keys.first] }
     # That ^^^ works, but this is equivalent:
-    # biggest = { letter_hash.keys.first => letter_hash.values.first }
-    # 
-    # letter_hash.each do |key, value|
-    #   if value > biggest.values.first 
-    #     biggest = {}
-    #     biggest[key] = value 
-    #     # Or, you could do biggest = {key => value}
-    #   end 
-    # end
-    # 
-    # biggest
+    biggest = { letter_hash.keys.first => letter_hash.values.first }
+    
+    letter_hash.each do |key, value|
+     if value > biggest.values.first 
+       biggest = {}
+       biggest[key] = value 
+       # Or, you could do biggest = {key => value}
+     end 
+    end
+    
+    biggest
     
     # I think I found a quicker way than above ^^^, with less code:
-    most_common = letter_hash.sort_by{ |key, value| value }.last 
+    # most_common = letter_hash.sort_by{ |key, value| value }.last 
     # That ^^^ sorts the hash by its values, then returns an array containing the letter that appears the most and its corresponding number of appearances. Ex: ["f", 13]
-    binding.pry
-    { most_common.first => most_common.last } # I'll have to compare the speeds of each method later. Originally, I set this to a variable, but that's not really needed.
+    # { most_common.first => most_common.last } # I'll have to compare the speeds of each method later. Originally, I set this to a variable, but that's not really needed.
+    
+    # Unfortunately, this doesn't work. It fails the tests because of that call to #last. 
+    # When there's a tie (multiple letters appearing an equal number of times), the FIRST letter that appears in the string should be returned. #last doesn't do that.
   end
 end
